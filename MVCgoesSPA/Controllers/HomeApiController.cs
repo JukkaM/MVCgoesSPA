48f1fa32d195cc52b785f154912fe4c895685621
Mähönen Jukka <jukka.mahonen@solteq.com>
2019-03-25 21:48:45 +0200

    added Home2Controller to host SPA page and new API controller

---
+++ MVCgoesSPA/Controllers/HomeApiController.cs
@@ -0,0 +1,43 @@
+﻿using MVCgoesSPA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCgoesSPA.Controllers
+{
+    public class HomeApiController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Spa> Get()
+        {
+            return new Repository().GetAllSpas();
+        }
+
+        // GET api/<controller>/5
+        public Spa Get(int id)
+        {
+            return new Repository().GetAllSpas().Skip(id - 1).First();
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Spa value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Spa value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
